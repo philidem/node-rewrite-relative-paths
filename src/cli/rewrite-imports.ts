185adb8b4eae90d4ae82f4d567e7fb2093a8e9e2
Phil Gates-Idem <phil.gates-idem@jupiterone.com>
2020-11-25 01:06:17 -0500

    Do not make monorepo required

--- src/cli/rewrite-imports.ts
+++ src/cli/rewrite-imports.ts
@@ -11,7 +11,7 @@ export async function run() {
     })
     .option('monorepo', {
       type: 'boolean',
-      required: true,
+      required: false,
     })
     .option('h', {
       alias: 'help',
